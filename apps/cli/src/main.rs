ab084128f993cf8e80b07cb6d4ae05e873402500
Deadman <deadman@noreply.codeberg.org>
2025-12-10 15:48:54 +1030

    OS Detecting

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,8 +1,39 @@
-extern crate tld_mod_manager_core;
+use std::env;
 
+use clap::Parser;
 use tld_mod_manager_core::testing;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
-    testing();
+    let current_os = env::consts::OS;
+    match current_os {
+        "windows" => println!("User is on Windows!"),
+        "macos" => println!("User is on macOS!"),
+        "linux" => println!("User is on Linux!"),
+        _ => println!("Unknown operating system"),
+    }
+
+    let test = testing();
+    if let Err(_) = test {
+        eprintln!("Failed to find steam path");
+    } else {
+        println!("{:?}", test)
+    }
+
+    // println!("{}", test)
+
+    // let args = Args::parse();
+
+    // for _ in 0..args.count {
+    //     println!("Hello {}!", args.name);
+    // }
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,17 +1,18 @@
 use std::io;
-use winreg::enums::*;
 use winreg::RegKey;
+use winreg::enums::*;
 
-pub fn testing() -> io::Result<()> {
-    println!("Reading some system info...");
-
+pub fn testing() -> Result<RegKey, io::Error> {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let key = hklm.open_subkey("SOFTWARE\\WOW6432NODE\\Valve\\Steam")?;
-    // let key = hklm.open_subkey("SOFTWARE\\Valve\\Steam");
-    let value: String = key.get_value("InstallPath")?;
+    Ok(key)
 
-    println!("{}", value);
-    Ok(())
+    // let key = hklm.open_subkey("SOFTWARE\\Valve\\Steam");
+    // let value = testing2.get_value("InstallPath");
+    // let testing = match value {
+    //     Ok(test) => test,
+    //     Err(e) => return Err(e),
+    // };
 }
 
 pub fn add(left: u64, right: u64) -> u64 {
