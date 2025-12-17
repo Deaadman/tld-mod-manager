071987e1c682f55d47101eb2d1db19c281633ef5
Deadman <deadman@noreply.codeberg.org>
2025-12-18 10:17:27 +1030

    Function to determine MelonLoader version on Windows

--- core/src/melonloader.rs
+++ core/src/melonloader.rs
@@ -1,8 +1,15 @@
 pub mod melonloader {
     use std::path::PathBuf;
 
-    pub mod version {
-        pub fn get_version() {}
+    #[cfg(target_os = "windows")]
+    pub fn get_version(game_dir: PathBuf) -> VersionInfo {
+        use win32_version_info::VersionInfo;
+
+        let melonloader_dir = game_dir.join("MelonLoader");
+        let melonloader_assembly_dir = game_dir.join("MelonLoader.dll");
+
+        let info = VersionInfo::from_file(melonloader_assembly_dir).expect("Failed to retrieve version information");
+        return info.file_version;
     }
 
     pub fn installed(game_dir: PathBuf) -> bool {
